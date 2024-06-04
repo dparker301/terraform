5164f617e7cbd493e03e16bd1a42114720a8ce3c
dparker301 <mr.leonard.parker@gmail.com>
2024-06-04 13:29:57 -0400

    Resolved merge conflicts and staged changes

--- README.md
+++
@@ -1,1 +0,0 @@
-# My Project_ Input Variables Block
--- main.tf
+++ main.tf
@@ -1,6 +1,33 @@
 # Configure the AWS Provider
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
+}
+
+# Terraform Data Block - To Lookup Latest Ubuntu 20.04 AMI Image
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"]
+}
+
+# Terraform Resource Block - To Build EC2 instance in Public Subnet
+resource "aws_instance" "web_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  #subnet_id     = aws_subnet.public_subnets["public_subnet_1"].id
+  tags = {
+    Name = "Ubuntu EC2 Server"
+  }
 }
 
 # Declare the VPC
@@ -36,3 +63,14 @@ resource "aws_route_table" "public_route_table" {
 }
 
 
+resource "aws_subnet" "variables-subnet" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.variables_sub_cidr
+  availability_zone       = var.variables_sub_az
+  map_public_ip_on_launch = var.variables_sub_auto_ip
+
+  tags = {
+    Name      = "sub-variables-${var.variables_sub_az}"
+    Terraform = "true"
+  }
+}
